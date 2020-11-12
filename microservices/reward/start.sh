5fb7a99dd2e69bc886543808b0673344f8580913
Berker Batur <berkerbatur@gmail.com>
2020-11-12 16:43:24 +0300

    Wait for postgres added to microservices to prevent them connection failure when containers up.

---
+++ microservices/reward/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# ugly way to wait postgres to be up :)
+sleep 5
+node server.js
---
+++ microservices/reward/wait-for-postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for-postgres.sh
+# sleep at least 4 seconds first
+sleep 4
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
